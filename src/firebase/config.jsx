73cb9e03782d46349f8e9506ec4f02e0f9ac9f92
Leek088 <leonardoleeko88@gmail.com>
2024-06-29 21:28:06 -0300

    Adicionar arquivos de projeto.

---
+++ src/firebase/config.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1WVTCVroUWYUv0Rk3C6lEe-a5feGVTYo",
+    authDomain: "miniblog-a1bfc.firebaseapp.com",
+    projectId: "miniblog-a1bfc",
+    storageBucket: "miniblog-a1bfc.appspot.com",
+    messagingSenderId: "821990872299",
+    appId: "1:821990872299:web:994722ed4fba8d20489aae"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db }
